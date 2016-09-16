4fe8f7213820bcdc71567837b65d920a964ee1f2
Cedric Champeau <cedric.champeau@gmail.com>
2016-09-16 10:22:51 +0200

    Add experimental cross-platform compilation with compile avoidance

--- buildSrc/src/main/groovy/CompileAvoidance.groovy
+++ buildSrc/src/main/groovy/CompileAvoidance.groovy
@@ -1,12 +1,10 @@
-import groovy.transform.CompileDynamic
-import groovy.transform.CompileStatic
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.jvm.tasks.api.ApiJar
-
-@CompileStatic
+//@CompileStatic
 class CompileAvoidance implements Plugin<Project> {
     void apply(Project project) {
         ApiExtension extension = (ApiExtension) project.extensions.create("api", ApiExtension)
@@ -17,29 +15,50 @@ class CompileAvoidance implements Plugin<Project> {
             if (!targetPlatforms) {
                 targetPlatforms << 'java7'
             }
-            println "Target platforms for $project.name: ${targetPlatforms}"
+            def compileConfiguration = project.configurations.getByName('compile')
+            List<JavaCompile> compileTasks = project.tasks.withType(JavaCompile).collect()
+            compileTasks.each { JavaCompile compileTask ->
+                targetPlatforms.each { String platform ->
+                    def capitalizedPlatform = platform.capitalize()
+                    def taskName = "${compileTask.name}$capitalizedPlatform"
+                    def compilePlatformConfiguration = project.configurations.findByName("compileClasspath${ capitalizedPlatform}")?:project.configurations.create("compileClasspath${capitalizedPlatform}")
+                    compilePlatformConfiguration.attributes(type: 'api', platform: platform)
+                    compilePlatformConfiguration.extendsFrom compileConfiguration
+                    def platformCompile = project.tasks.create(taskName, JavaCompile, new Action<JavaCompile>() {
+                        @Override
+                        void execute(final JavaCompile task) {
+                            task.options.fork = true
+                            def level = "1.${platform - 'java'}"
+                            task.options.forkOptions.executable = "/opt/jdk${level}.0/bin/javac"
+                            task.sourceCompatibility = level
+                            task.targetCompatibility = level
+                            task.source = compileTask.source
+                            task.destinationDir = project.file("$project.buildDir/classes/$platform/$taskName")
+                            task.classpath = compilePlatformConfiguration
+                        }
+                    })
+                    def apiJar = project.tasks.create("${taskName}ApiJar", ApiJar, { ApiJar apiJar ->
+                        apiJar.outputFile = project.file("$project.buildDir/api/${project.name}-${taskName}.jar")
+                        apiJar.inputs.dir(platformCompile.destinationDir).skipWhenEmpty()
+                        apiJar.exportedPackages = extension.exports
+                        apiJar.dependsOn(platformCompile)
+                    } as Action)
+                    project.artifacts.add(compilePlatformConfiguration.name, [file: apiJar.outputFile, builtBy: apiJar])
 
-            def compileConfiguration = project.configurations.getByName('compileClasspath')
-            compileConfiguration.attributes(type: 'api')
-
-            project.tasks.withType(JavaCompile).all { JavaCompile compileTask ->
-                def apiJar = project.tasks.create("${compileTask.name}ApiJar", ApiJar, { ApiJar apiJar ->
-                    apiJar.outputFile = project.file("$project.buildDir/api/${project.name}-${compileTask.name}.jar")
-                    apiJar.inputs.dir(compileTask.destinationDir).skipWhenEmpty()
-                    apiJar.exportedPackages = extension.exports
-                    apiJar.dependsOn(compileTask)
-                } as Action)
-                attachArtifact(project, apiJar)
+                    if (!compileTask.name.contains('Test')) {
+                        def jar = project.tasks.create("${platform}Jar", Jar) { Jar jar ->
+                            jar.from project.files(platformCompile.destinationDir)
+                            jar.destinationDir = project.file("$project.buildDir/libs")
+                            jar.classifier = platform
+                            jar.dependsOn(platformCompile)
+                        }
+                        project.tasks.getByName('build').dependsOn jar
+                    }
+                }
             }
+
         }
 
-    }
-
-    @CompileDynamic
-    void attachArtifact(Project p, ApiJar jar) {
-        p.artifacts {
-            compileClasspath file: jar.outputFile, builtBy: jar
-        }
     }
 
 }
